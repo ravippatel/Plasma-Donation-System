2b06ee0ef95393d14c5e1a21be3470e0716868a9
Trunali Dumbre <dumbre.t@northeastern.edu>
2021-04-13 16:24:45 -0400

    Project phase 1A

---
+++ Plasma_Blood_Donation/src/Business/Enterprise/LegalEnterprise.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+
+public class LegalEnterprise extends Enterprise {
+    
+    public LegalEnterprise(String name){
+        super(name,EnterpriseType.Legal);
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+    
+}
